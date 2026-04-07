36f20723348982d3f34ae7d6b0466dd17d1ca3a0
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-04-07 17:12:21 +0000

    Removed the file

--- f1.py
+++
@@ -1,6 +0,0 @@
-print ("Nothing is True Everything is Permitted")
-print ("My name is Gustavo but you can call me Gus")
-print ("Beware whou you Trust Sergeant people you know can hurt you the most")
-
-
-

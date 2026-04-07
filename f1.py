6f447060dad138d417a0586fda9c8a37fa31cf10
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-04-07 11:57:02 +0000

    Second stash added

--- f1.py
+++ f1.py
@@ -1,2 +1,4 @@
 print ("Nothing is True Everything is Permitted")
+
 print ("My name is Gustavo but you can call me Gus")
+print ("Beware whou you Trust Sergeant people you know can hurt you the most")

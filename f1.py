99763692564a02918d9ed99ea82f6d03ada355f5
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-04-07 12:00:08 +0000

    changed

--- f1.py
+++ f1.py
@@ -1,4 +1,8 @@
 print ("Nothing is True Everything is Permitted")
+<<<<<<< Updated upstream
 
 print ("My name is Gustavo but you can call me Gus")
 print ("Beware whou you Trust Sergeant people you know can hurt you the most")
+=======
+print ("My name is Gustavo but you can call me Gus")
+>>>>>>> Stashed changes

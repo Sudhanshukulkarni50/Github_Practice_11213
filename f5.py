b0f4db94263ec0570554548f6d0b9ac648add032
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-04-13 13:38:29 +0000

    local_changes_1221

--- f5.py
+++ f5.py
@@ -1,5 +1,9 @@
 print ("Good Evening Cunts!!")
 print ("Woher Kommst du?")
 print ("Halo Ich Bin Sudhanshu")
+<<<<<<< HEAD
 print("Halo Ich Auch Nischt Gut Danke")
 
+=======
+print ("Ich Komme Aus Pune")
+>>>>>>> 407ae27 (added a language)
